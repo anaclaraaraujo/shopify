2569d99d30ab40769223bf63f135a461e05b06ab
anaclaraaraujo <helloanaclaraaraujo@gmail.com>
2025-07-16 19:15:24 -0300

    chore: setup tailwind css and shadcn/ui

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen min-w-full bg-slate-500">
+      <h2 className="text-4xl text-red-400">hello world!</h2>
+    </div>
+  );
+}
