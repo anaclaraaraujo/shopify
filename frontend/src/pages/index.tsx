53b8862cebd3537e4e36d59e76c729d848dbb0ff
anaclaraaraujo <helloanaclaraaraujo@gmail.com>
2025-07-16 19:44:01 -0300

    feat: product list page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,26 @@
 export default function Home() {
   return (
-    <article className="flex flex-col gap-10 md:gap-20">
-      <h1 className="text-white py-24">hello</h1>
-    </article>
+    <div className="flex flex-col py-24 flex-grow h-full">
+      <header className="">
+        <div className="container space-y-6 flex flex-col items-start justify-between md:flex-row md:items-end lg:items-end">
+          <div className="flex flex-col gap-4 md:px-0">
+            {/* TAG */}
+            <span className="text-body-tag text-cyan-100 w-fit rounded-md text-center md:text-left py-2 px-4 bg-cyan-300">
+              PRODUTOS
+            </span>
+
+            {/* Titulo */}
+            <h1 className="text-balance text-start md:text-left text-heading-lg md:text-heading-xl max-w-2xl text-gray-100">
+              Produtos esportivos para impulsionar seu treino
+            </h1>
+          </div>
+        </div>
+      </header>
+
+      {/* Search */}
+
+      {/* Listagem dos produtos */}
+
+    </div>
   );
 }
